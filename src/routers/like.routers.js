54299e6fa50f01a15c2db589badbe1cff6adedea
Quyet16 <quyetcoder16@gmail.com>
2024-01-05 19:14:41 +0700

    change struct api

--- src/routers/like.routers.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getListLikeByResTaurant } from "../controllers/like.controllers.js";
-
-const likeRouters = express.Router();
-
-likeRouters.get("/get-list-like-by-restaurant/:res_id",getListLikeByResTaurant);
-
-export default likeRouters;
--- src/routers/root.routers.js
+++ src/routers/root.routers.js
@@ -1,8 +1,6 @@
 import express from "express";
-import likeRouters from "./like.routers.js";
 
 const rootRouters = express.Router();
 
-rootRouters.use("/like", likeRouters);
 
 export default rootRouters;
